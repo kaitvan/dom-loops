f5aa181abbbb9bb7ee2108d78d15f6e42cf71a8e
kaitvan <kaitlyn.vanhook@gmail.com>
2020-07-21 21:18:37 -0500

    dynamic cards created through javascript

--- loops.js
+++ loops.js
@@ -6,31 +6,31 @@
 
 // for (setup, condition, increment) {} keeps looping until the condition is met, and increments as given
 
-const colors = ['red', 'orange', 'blue', 'yellow', 'green']
+// const colors = ['red', 'orange', 'blue', 'yellow', 'green']
 
-const colorLoop = () => {
-    let domString = '';
+// const colorLoop = () => {
+//     let domString = '';
 
-    for (let i = 0; i < colors.length; i++) {
-        domString += `<h1>${colors[i]}</h1>`;
-    }
-    console.log(domString)
-}
+//     for (let i = 0; i < colors.length; i++) {
+//         domString += `<h1>${colors[i]}</h1>`;
+//     }
+//     console.log(domString)
+// }
 
-colorLoop()
+// colorLoop()
 
-// for in loop works for objects; for of loops could work too
+// // for in loop works for objects; for of loops could work too
 
-const colorLoop2 = () => {
-    let domString = '';
-    let color;
-    for (color of colors) {
-        domString += `<h1>${color}</h1>`;
-    }
-    console.log(domString)
-}
+// const colorLoop2 = () => {
+//     let domString = '';
+//     let color;
+//     for (color of colors) {
+//         domString += `<h1>${color}</h1>`;
+//     }
+//     console.log(domString)
+// }
 
-colorLoop2()
+// colorLoop2()
 
 
 
